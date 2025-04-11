a74fdda9e8b2ccac15dc16f361b34d6aa5cc37cf
MaykonAlves <MaykonAlves@DESKTOP-B3VDBQE>
2025-04-11 01:14:18 -0300

    Adicionar arquivos de projeto.

---
+++ JogoForca/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace JogoForca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt8 = new System.Windows.Forms.TextBox();
+            this.txt7 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt12 = new System.Windows.Forms.TextBox();
+            this.txt11 = new System.Windows.Forms.TextBox();
+            this.txt10 = new System.Windows.Forms.TextBox();
+            this.txt9 = new System.Windows.Forms.TextBox();
+            this.btnAdivinhar = new System.Windows.Forms.Button();
+            this.txtLetra = new System.Windows.Forms.MaskedTextBox();
+            this.PicForca = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.lblTentativas = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PicForca)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Enabled = false;
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(300, 180);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(26, 26);
+            this.txt1.TabIndex = 0;
+            this.txt1.Visible = false;
+            // 
+            // txt2
+            // 
+            this.txt2.Enabled = false;
+            this.txt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.Location = new System.Drawing.Point(332, 180);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(26, 26);
+            this.txt2.TabIndex = 4;
+            this.txt2.Visible = false;
+            // 
+            // txt4
+            // 
+            this.txt4.Enabled = false;
+            this.txt4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt4.Location = new System.Drawing.Point(395, 180);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(26, 26);
+            this.txt4.TabIndex = 6;
+            this.txt4.Visible = false;
+            // 
+            // txt3
+            // 
+            this.txt3.Enabled = false;
+            this.txt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt3.Location = new System.Drawing.Point(363, 180);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(26, 26);
+            this.txt3.TabIndex = 5;
+            this.txt3.Visible = false;
+            // 
+            // txt8
+            // 
+            this.txt8.Enabled = false;
+            this.txt8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt8.Location = new System.Drawing.Point(522, 180);
+            this.txt8.Name = "txt8";
+            this.txt8.Size = new System.Drawing.Size(26, 26);
+            this.txt8.TabIndex = 10;
+            this.txt8.Visible = false;
+            // 
+            // txt7
+            // 
+            this.txt7.Enabled = false;
+            this.txt7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt7.Location = new System.Drawing.Point(490, 180);
+            this.txt7.Name = "txt7";
+            this.txt7.Size = new System.Drawing.Size(26, 26);
+            this.txt7.TabIndex = 9;
+            this.txt7.Visible = false;
+            // 
+            // txt6
+            // 
+            this.txt6.Enabled = false;
+            this.txt6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt6.Location = new System.Drawing.Point(459, 180);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(26, 26);
+            this.txt6.TabIndex = 8;
+            this.txt6.Visible = false;
+            // 
+            // txt5
+            // 
+            this.txt5.Enabled = false;
+            this.txt5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt5.Location = new System.Drawing.Point(427, 180);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(26, 26);
+            this.txt5.TabIndex = 7;
+            this.txt5.Visible = false;
+            // 
+            // txt12
+            // 
+            this.txt12.Enabled = false;
+            this.txt12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt12.Location = new System.Drawing.Point(648, 180);
+            this.txt12.Name = "txt12";
+            this.txt12.Size = new System.Drawing.Size(26, 26);
+            this.txt12.TabIndex = 14;
+            this.txt12.Visible = false;
+            // 
+            // txt11
+            // 
+            this.txt11.Enabled = false;
+            this.txt11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt11.Location = new System.Drawing.Point(616, 180);
+            this.txt11.Name = "txt11";
+            this.txt11.Size = new System.Drawing.Size(26, 26);
+            this.txt11.TabIndex = 13;
+            this.txt11.Visible = false;
+            // 
+            // txt10
+            // 
+            this.txt10.Enabled = false;
+            this.txt10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt10.Location = new System.Drawing.Point(585, 180);
+            this.txt10.Name = "txt10";
+            this.txt10.Size = new System.Drawing.Size(26, 26);
+            this.txt10.TabIndex = 12;
+            this.txt10.Visible = false;
+            // 
+            // txt9
+            // 
+            this.txt9.Enabled = false;
+            this.txt9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt9.Location = new System.Drawing.Point(553, 180);
+            this.txt9.Name = "txt9";
+            this.txt9.Size = new System.Drawing.Size(26, 26);
+            this.txt9.TabIndex = 11;
+            this.txt9.Visible = false;
+            // 
+            // btnAdivinhar
+            // 
+            this.btnAdivinhar.Location = new System.Drawing.Point(553, 294);
+            this.btnAdivinhar.Name = "btnAdivinhar";
+            this.btnAdivinhar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdivinhar.TabIndex = 15;
+            this.btnAdivinhar.Text = "Advinhar";
+            this.btnAdivinhar.UseVisualStyleBackColor = true;
+            this.btnAdivinhar.Click += new System.EventHandler(this.btnAdivinhar_Click);
+            // 
+            // txtLetra
+            // 
+            this.txtLetra.Location = new System.Drawing.Point(518, 296);
+            this.txtLetra.Name = "txtLetra";
+            this.txtLetra.Size = new System.Drawing.Size(29, 20);
+            this.txtLetra.TabIndex = 16;
+            this.txtLetra.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLetra_KeyPress);
+            // 
+            // PicForca
+            // 
+            this.PicForca.BackColor = System.Drawing.Color.Transparent;
+            this.PicForca.Location = new System.Drawing.Point(134, 113);
+            this.PicForca.Name = "PicForca";
+            this.PicForca.Size = new System.Drawing.Size(114, 200);
+            this.PicForca.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicForca.TabIndex = 3;
+            this.PicForca.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(41, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(197, 300);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(515, 277);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Escolha uma letra";
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Location = new System.Drawing.Point(553, 343);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnReiniciar.TabIndex = 18;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Visible = false;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // lblTentativas
+            // 
+            this.lblTentativas.AutoSize = true;
+            this.lblTentativas.BackColor = System.Drawing.Color.Transparent;
+            this.lblTentativas.Location = new System.Drawing.Point(305, 235);
+            this.lblTentativas.Name = "lblTentativas";
+            this.lblTentativas.Size = new System.Drawing.Size(0, 13);
+            this.lblTentativas.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTentativas);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLetra);
+            this.Controls.Add(this.btnAdivinhar);
+            this.Controls.Add(this.txt12);
+            this.Controls.Add(this.txt11);
+            this.Controls.Add(this.txt10);
+            this.Controls.Add(this.txt9);
+            this.Controls.Add(this.txt8);
+            this.Controls.Add(this.txt7);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.PicForca);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PicForca)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox PicForca;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt8;
+        private System.Windows.Forms.TextBox txt7;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt12;
+        private System.Windows.Forms.TextBox txt11;
+        private System.Windows.Forms.TextBox txt10;
+        private System.Windows.Forms.TextBox txt9;
+        private System.Windows.Forms.Button btnAdivinhar;
+        private System.Windows.Forms.MaskedTextBox txtLetra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.Label lblTentativas;
+    }
+}
+
---
+++ JogoForca/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace JogoForca
+{
+    public partial class Form1 : Form
+    {
+        private static string palavra ;
+        private char[] revelado;
+        private int erros = 0;
+        string[] palavrasaleatoria = { "Cachorro", "Gato", "Papagaio", "Coelho", "Pato", "Leopardo" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtLetra_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Impede a entrada do caractere
+            }
+        }
+
+        private void btnAdivinhar_Click(object sender, EventArgs e)
+        {
+            int modificado = 0;
+            for (int i = 0; i < palavra.Length; i++) {
+
+                String nomeTxt = "txt" + (i + 1);
+                TextBox txt = this.Controls.Find(nomeTxt, true).FirstOrDefault() as TextBox;
+                txt.Visible = true;
+                
+                if (txtLetra.Text.ToLower() == palavra[i].ToString().ToLower())
+                {
+                   
+                    txt.Text = palavra[i].ToString();
+                    revelado[i] = palavra[i];
+                    modificado++;
+                }
+                
+            }
+
+            if (modificado == 0) {
+                erros++;
+                var imagem = (System.Drawing.Image)Properties.Resources.ResourceManager.GetObject((erros).ToString());
+                PicForca.Image = imagem;
+            }
+            verificarGanhou(revelado, palavra);
+            verificarPerdeu(erros);
+            lblTentativas.Text = lblTentativas.Text +txtLetra.Text+",";
+
+            txtLetra.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            Random rnd = new Random();
+            palavra = palavrasaleatoria[rnd.Next(0,palavrasaleatoria.Length)];
+            revelado = new char [palavra.Length]; ;
+
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                revelado[i] = '_';
+                String nomeTxt = "txt" + (i + 1);
+                TextBox txt = this.Controls.Find(nomeTxt, true).FirstOrDefault() as TextBox;
+                txt.Visible = true;
+
+            }
+        }
+
+        private void verificarGanhou(char[] revelados, string palavra) {
+            string palavrarevelada="";
+            for (int i = 0; i < palavra.Length; i++) {
+                palavrarevelada = palavrarevelada + revelado[i].ToString();
+            }
+            if (palavrarevelada == palavra) {
+
+                MessageBox.Show("Você venceu");
+                btnAdivinhar.Enabled = false;
+                btnReiniciar.Visible = true;
+
+            }
+        
+        }
+
+        private void verificarPerdeu(int erros) {
+            if (erros == 6) {
+                MessageBox.Show("Você perdeu");
+                btnAdivinhar.Enabled = false;
+                btnReiniciar.Visible = true;
+            }
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            sumirTodosTxt();
+            PicForca.Image = null;
+            btnAdivinhar.Enabled = true;
+            btnReiniciar.Visible = false;
+            lblTentativas.Text = "";
+            erros = 0;
+            Random rnd = new Random();
+            palavra = palavrasaleatoria[rnd.Next(0, palavrasaleatoria.Length)];
+            revelado = new char[palavra.Length]; ;
+
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                revelado[i] = '_';
+                String nomeTxt = "txt" + (i + 1);
+                TextBox txt = this.Controls.Find(nomeTxt, true).FirstOrDefault() as TextBox;
+                txt.Visible = true;
+
+            }
+        }
+
+        private void sumirTodosTxt() {
+            for (int i = 1; i <= 12; i++) {
+                String nomeTxt = "txt" + (i);
+                TextBox txt = this.Controls.Find(nomeTxt, true).FirstOrDefault() as TextBox;
+                txt.Clear();
+                txt.Visible = false;
+            }
+        }
+    }
+}
